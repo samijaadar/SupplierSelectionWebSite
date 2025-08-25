bb050a46488bd790ffd23a1e608769e89fd8ca4b
samijaadar <jaadar.sami@gmail.com>
2025-08-25 19:05:30 +0200

    test

--- dashboard.py
+++ dashboard.py
@@ -9,17 +9,6 @@ import asyncio
 import streamlit as st
 from concurrent.futures import ThreadPoolExecutor
 
-# Define a function to run sync code in a thread
-executor = ThreadPoolExecutor()
-
-
-async def run_system_async(system, df, company, mail):
-    loop = asyncio.get_running_loop()
-
-    # Use a lambda or functools.partial to pass the function without calling it
-    result = await loop.run_in_executor(executor, lambda: system.rank(df, company, mail))
-
-    return result
 
 def dashboard_page():
 
@@ -163,7 +152,5 @@ def dashboard_page():
             weights = dict(zip(edited_df['Criterion'], edited_df['Weight']))
 
             system = SupplierRankingSystem(beneficial, non_beneficial, weights)
-            with st.spinner("Running system..."):
-                asyncio.run(run_system_async(system, df, company_name, contact_email))
-            # Streamlit success message
+            system.rank(system, df, company_name, contact_email)
             st.success(f"Uploaded: `{folder_name}`")
